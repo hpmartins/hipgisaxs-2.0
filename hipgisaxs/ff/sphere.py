65df12740036e2d25d1331ae7439cee9054cbadf
hpmartins <hpmartins@gmail.com>
2025-07-29 12:20:50 -0700

    Fixed form-factor
    - Added missing factor of 3
    - Added mask to filter q=0 where ff = volume

--- hipgisaxs/ff/sphere.py
+++ hipgisaxs/ff/sphere.py
@@ -2,10 +2,16 @@
 
 import numpy as np
 
-def sphere(qx, qy, qz, radius):
 
-    vol = 4/3 * np.pi * radius**3
+def sphere(qx, qy, qz, radius):
+    vol = (4 / 3) * np.pi * radius**3
     qR = np.sqrt(qx**2 + qy**2 + qz**2) * radius
     tmp = np.exp(1j * qz * radius)
-    ff =  vol * tmp *  (np.sin(qR) - qR * np.cos(qR)) / qR**3
+
+    ff = np.ones_like(qR) * vol
+    mask = qR != 0
+
+    term = np.sin(qR[mask]) - qR[mask] * np.cos(qR[mask])
+    ff[mask] = (3 * vol * tmp[mask] * term) / qR[mask] ** 3
+
     return ff
