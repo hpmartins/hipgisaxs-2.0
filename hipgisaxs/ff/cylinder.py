363e84eb8b9e7431f2f2be57aaa1d36fd9d1db9f
hpmartins <hpmartins@gmail.com>
2025-07-29 12:19:00 -0700

    Fixed form-factor
    - Missing factor of 2
    - Changed np.sinc to sin(x)/x since np.sinc is sin(πx)/(πx)

--- hipgisaxs/ff/cylinder.py
+++ hipgisaxs/ff/cylinder.py
@@ -2,16 +2,19 @@ import numpy as np
 from scipy.special import j1
 from ..rotation import rotate
 
-def cylinder(qx, qy, qz, radius, height, orientation = None):
 
+def cylinder(qx, qy, qz, radius, height, orientation=None):
     if orientation is None:
         q1, q2, q3 = qx.ravel(), qy.ravel(), qz.ravel()
     else:
         q1, q2, q3 = rotate(qx, qy, qz, orientation)
-   
+
     vol = np.pi * radius**2 * height
     qpR = np.sqrt(q1**2 + q2**2) * radius
-    f1 =  np.sinc(q3 * height * 0.5)
+    qparH = q3 * height / 2
+
+    f1 = np.sin(qparH) / qparH
     f2 = j1(qpR) / qpR
-    f3 = np.exp(1j * 0.5 * height * q3)
-    return (vol * f1 * f2 * f3)
+    f3 = np.exp(1j * height * q3 / 2)
+
+    return 2 * vol * f1 * f2 * f3
