1aeefa32f6312ee8cd117549e9cbd925be67fb4b
Henrique <hpmartins@gmail.com>
2025-08-01 10:33:31 -0700

    Fixed form-factor
    - np.sinc arg must be divided by pi

--- hipgisaxs/ff/cuboid.py
+++ hipgisaxs/ff/cuboid.py
@@ -2,15 +2,19 @@ import numpy as np
 
 from ..rotation import rotate
 
-def cuboid(qx, qy, qz, length, width, height, orientation = None):
+
+def cuboid(qx, qy, qz, length, width, height, orientation=None):
 
     if orientation is None:
         q1, q2, q3 = qx.ravel(), qy.ravel(), qz.ravel()
     else:
         q1, q2, q3 = rotate(qx, qy, qz, orientation)
-   
-    vol = length * width * height
-    shft = np.exp(1j * q3 * height / 2)
-    ft = np.sinc(q1*length/2)*np.sinc(q2*width/2)*np.sinc(q3*height/2)
-    return (vol * ft * shft)
 
+    volume = length * width * height
+    z_shift = np.exp(1j * q3 * height / 2)
+
+    arg1 = q1 * (length / 2) / np.pi
+    arg2 = q2 * (width / 2) / np.pi
+    arg3 = q3 * (height / 2) / np.pi
+
+    return volume * z_shift * np.sinc(arg1) * np.sinc(arg2) * np.sinc(arg3)
