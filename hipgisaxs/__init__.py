0918fd12679721ba882e84585ee57c68d362d106
hpmartins <hpmartins@gmail.com>
2025-08-13 23:46:37 -0700

    Improved multilayer features
    - Fixed missing sign in kz (following C++ implementation)
    - If first provided layer is vacuum, remove duplicated vacuum layer
    - Masking alpha <= 0 when calculating Fresnel coefficients
    - Casting arrays to np.complex128
    - changed variable one_n2 to one_minus_n2 for clarity
    - Using wavelength as parameter instead of k0

--- hipgisaxs/__init__.py
+++ hipgisaxs/__init__.py
@@ -1,4 +1,5 @@
 from .structure_factor import structure_factor
 from .fresnel import propagation_coeffs
+from .fresnel_multi import MultiLayer, Layer
 from .detector import Detector
 from .unitcell import Unitcell
--- hipgisaxs/fresnel_multi.py
+++ hipgisaxs/fresnel_multi.py
@@ -3,7 +3,7 @@ import numpy as np
 
 class Layer:
     def __init__(self, delta, beta, order, thickness):
-        self.one_n2 = 2 * complex(delta, beta)
+        self.one_minus_n2 = 2 * complex(delta, beta)
         self.order = order
         self.thickness = thickness
         self.zval = 0
@@ -18,15 +18,19 @@ class MultiLayer:
     def insert(self, layer):
         if not isinstance(layer, Layer):
             raise TypeError("only Layer types can be inserted into multilayered object")
-            exit(101)
         if not layer.order > 0:
             raise ValueError("the order of layer must be greater than 0")
-            exit(102)
         self.layers.insert(layer.order, layer)
 
     def setup_multilayer(self):
         if self._setup_:
             return
+
+        if len(self.layers) > 1 and self.layers[1].one_minus_n2 == 0:
+            self.layers = self.layers[1:]
+            self.layers[0].thickness = 0
+            for idx, layer in enumerate(self.layers):
+                layer.order = idx
 
         # put substrate at the end
         self.layers.append(self.substrate)
@@ -41,7 +45,7 @@ class MultiLayer:
         # run only once
         self._setup_ = True
 
-    def parratt_recursion(self, alpha, k0, order):
+    def parratt_recursion(self, alpha, wavelength, order):
         self.setup_multilayer()
         nlayer = len(self.layers)
         shape = np.shape(alpha)
@@ -57,13 +61,14 @@ class MultiLayer:
         dim2 = (nlayer,) + shape
 
         # cacl k-value
-        kz = np.zeros(dim2, np.complex_)
+        k0 = 2 * np.pi / wavelength
+        kz = np.zeros(dim2, np.complex128)
         for i in range(nlayer):
-            kz[i, :] = k0 * np.sqrt(sin_a**2 - self.layers[i].one_n2)
+            kz[i, :] = -k0 * np.sqrt(sin_a**2 - self.layers[i].one_minus_n2)
 
         # calculate Rs
-        R = np.zeros(dim2, dtype=np.complex_)
-        T = np.zeros(dim2, dtype=np.complex_)
+        R = np.zeros(dim2, dtype=np.complex128)
+        T = np.zeros(dim2, dtype=np.complex128)
         T[-1] = 1
         for i in reversed(range(nlayer - 1)):
             z = self.layers[i].zval
@@ -78,14 +83,18 @@ class MultiLayer:
 
         return T[order] / T0, R[order] / T0
 
-    def propagation_coeffs(self, alphai, alpha, k0, order):
-        Ti, Ri = self.parratt_recursion(alphai, k0, order)
-        Tf, Rf = self.parratt_recursion(alpha, k0, order)
-        fc = np.zeros((4,) + alpha.shape, np.complex_)
+    def propagation_coeffs(self, alphai, alpha, wavelength, order):
+        Ti, Ri = self.parratt_recursion(alphai, wavelength, order)
 
-        fc[0, :] = Ti * Tf
-        fc[1, :] = Ri * Tf
-        fc[2, :] = Ti * Rf
-        fc[3, :] = Ri * Rf
-        fc[:, alpha < 0] = 0
+        fc = np.zeros((4,) + alpha.shape, np.complex128)
+        mask = alpha > 0
+        if np.any(mask):
+            alpha_positive = alpha[mask]
+            Tf, Rf = self.parratt_recursion(alpha_positive, wavelength, order)
+
+            fc[0, mask] = Ti * Tf
+            fc[1, mask] = Ri * Tf
+            fc[2, mask] = Ti * Rf
+            fc[3, mask] = Ri * Rf
+
         return fc
