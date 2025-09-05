bc2bbf6867e97cc4a92ec7cfa2a37242fe99a737
hpmartins <hpmartins@gmail.com>
2025-09-05 12:21:21 -0700

    Standardized contrast calculation
    - Added n and n2 attributes to ShapeBase
    - CoreShell.ff() now has a n_ref parameter for correct contrast calculation
    - Unitcell.ff() calculates CoreShell.ff() using the layer n_ref

--- hipgisaxs/unitcell.py
+++ hipgisaxs/unitcell.py
@@ -15,7 +15,7 @@ except ImportError:
     warnings.warn("failed to import meshff, required for triangulated structures", stacklevel=2)
 
 
-def makeShapeObject(shape):
+def makeShapeObject(shape) -> "ShapeBase":
     fftype = shape["formfactor"]
     if fftype in globals():
         shape_args = {k: v for k, v in shape.items() if k != "formfactor"}
@@ -32,6 +32,9 @@ class ShapeBase(ABC):
         self.locations = locations
         self.orient = orient
 
+        self.n = complex(1 - self.delta, self.beta)
+        self.n2 = self.n**2
+
     def ff(self, qx, qy, qz):
         pass
 
@@ -47,18 +50,23 @@ class CoreShell(ShapeBase):
             orient=self.shell.orient,
         )
 
-    def ff(self, qx, qy, qz):
+    def ff(self, qx, qy, qz, n_ref=complex(1, 0)):
         ff_core = self.core.ff(qx, qy, qz)
-        refidx_core = complex(self.core.delta, self.core.beta)
         ff_shell = self.shell.ff(qx, qy, qz)
-        refidx_shell = complex(self.shell.delta, self.shell.beta)
-        return refidx_shell * ff_shell + (refidx_core - refidx_shell) * ff_core
+        contrast_shell = self.shell.n2 - n_ref**2
+        contrast_core = self.core.n2 - self.shell.n2
+        return contrast_shell * ff_shell + contrast_core * ff_core
 
 
 class Unitcell:
     def __init__(self, shapes, delta=0, beta=0):
-        self.shapes = []
+        self.shapes: list[ShapeBase] = []
+        self.delta = delta
+        self.beta = beta
+
         self.n = complex(1 - delta, beta)
+        self.n2 = self.n**2
+
         for shape in shapes:
             if shape["formfactor"] == "CoreShell":
                 self.shapes.append(CoreShell(shape["Core"], shape["Shell"]))
@@ -68,9 +76,12 @@ class Unitcell:
     def ff(self, qx, qy, qz):
         ff = np.zeros(qx.size, dtype=complex)
         for shape in self.shapes:
-            n = complex(1 - shape.delta, shape.beta)
-            dn2 = n**2 - self.n**2
-            shape_ff = shape.ff(qx, qy, qz)
+            if isinstance(shape, CoreShell):
+                shape_ff = shape.ff(qx, qy, qz, n_ref=self.n)
+            else:
+                contrast_particle = shape.n2 - self.n2
+                shape_ff = contrast_particle * shape.ff(qx, qy, qz)
+
             locs = shape.locations
             if locs is None:
                 locs = [{"x": 0, "y": 0, "z": 0}]
@@ -80,7 +91,7 @@ class Unitcell:
                 phase = np.exp(1j * (qx * loc["x"] + qy * loc["y"] + qz * loc["z"]))
                 tempff += phase
 
-            ff += shape_ff * tempff * dn2
+            ff += shape_ff * tempff
 
         return ff
 
